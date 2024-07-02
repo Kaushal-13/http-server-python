5a8e24caad49423ce32fe4207a90454f17778f42
Kaushal-13 <tiwarikaushal2012@gmail.com>
2024-07-02 19:52:20 +0530

    Test Second Stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@
 import socket
 
 
+ok_response = 'HTTP/1.1 200 OK\r\n\r\n'
+error_response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -16,9 +20,11 @@ def main():
         try:
 
             print(f'Connection from {client},{addr}')
-
+            data = client.recv(1024)
+            print(data)
         finally:
             print("Done")
+            client.close()
 
 
 if __name__ == "__main__":
